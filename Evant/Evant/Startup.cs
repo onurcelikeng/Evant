882837e5ff18556071080dcd93adf23501278147
Onur Celik <onrcelik@outlook.com>
2018-01-24 23:52:41 +0300

    auth added for swagger ui

--- Evant/Evant/Startup.cs
+++ Evant/Evant/Startup.cs
@@ -70,6 +70,13 @@ namespace Evant
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "Evant API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "token"
+                });
             });
         }
 
@@ -89,7 +96,6 @@ namespace Evant
 
             app.UseMvc();
         }
-
 
     }
 }
