2dbbf51170ec20bc8151386fa0bb09c38d84b735
Elif Seray Dönmez <elifseraydonmez@gmail.com>
2018-02-13 16:36:57 +0300

    iformfile

--- Evant/Evant.Storage/Extensions/IFormFileExtensions.cs
+++ Evant/Evant.Storage/Extensions/IFormFileExtensions.cs
@@ -7,19 +7,19 @@ namespace Evant.Storage.Extensions
 {
     public static class IFormFileExtensions
     {
-        public static string GetFilename(this IFormFile file)
+        public static string GetFilename(IFormFile file)
         {
             return ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.ToString().Trim('"');
         }
 
-        public static async Task<MemoryStream> GetFileStream(this IFormFile file)
+        public static async Task<MemoryStream> GetFileStream(IFormFile file)
         {
             MemoryStream filestream = new MemoryStream();
             await file.CopyToAsync(filestream);
             return filestream;
         }
 
-        public static async Task<byte[]> GetFileArray(this IFormFile file)
+        public static async Task<byte[]> GetFileArray(IFormFile file)
         {
             MemoryStream filestream = new MemoryStream();
             await file.CopyToAsync(filestream);
